2a3a91c8af04958ee005c31e3ccca7a38b4b45a4
Abdallah <abdallahgxr@gmail.com>
2019-05-14 21:11:05 +0200

    scraping 1000 cars on cragslist

--- all_cars_scrape.py
+++ all_cars_scrape.py
@@ -3,43 +3,50 @@ import requests
 import lxml
 import pandas as pd 
 
-count = 0
 
-while count <= 3:
 
-    cars_url = 'https://losangeles.craigslist.org/search/sss?query=cars&sort=rel'
+cars_url_start = 'https://losangeles.craigslist.org/search/sss?query=cars&sort=rel'
+next_url = 'https://losangeles.craigslist.org{}'
 
-    cragslist_page = requests.get(cars_url)
-    if cragslist_page.status_code == requests.codes.ok:
+cragslist_page = requests.get(cars_url_start)
+if cragslist_page.status_code == requests.codes.ok:
 
-        soup = BeautifulSoup(cragslist_page.text, "lxml")
-    else:
-        print("request code denied")
+    soup = BeautifulSoup(cragslist_page.text, "lxml")
+else:
+    print("request code denied")
 
+cars_posts = soup.find('ul', class_='rows')
+#List of all the cars post on craiglist
+all_cars_list = cars_posts.find_all('li', class_='result-row')
+print(all_cars_list[0])
+print("_____________________________________________________\n")
+
+
+while len(all_cars_list) <= 1000: 
+    next_button = str(soup.find('span', class_='buttons').find('a', class_='button next')['href'])
+    soup = BeautifulSoup(requests.get(next_url.format(next_button)).text, 'lxml')
     cars_posts = soup.find('ul', class_='rows')
     all_cars = cars_posts.find_all('li', class_='result-row')
-    print(all_cars[0])
-    print("_____________________________________________________\n")
+    for car in all_cars:
+         all_cars_list.append(car)
+    print(all_cars[0].find('span', class_='result-price'))
+    print(next_button)
 
-    # infor mation of the car in car info are the title and the date 
-    next_button = soup.find('span', class_='buttons').find('a', class_='button next')
-    button_url = "https://losangeles.craigslist.org" + next_button['href']
-    print(type(button_url))
-    cars_url = button_url 
-    count +=1 
 
-print(cars_url)
-print(button_url)
-print(len(all_cars))
+# infor mation of the car in car info are the title and the date 
+
+print(next_button)
+
+print(len(all_cars_list))
 #---------- Needed Results ---------------------------# 
-"""
+
 cragslist_cars_data = {
     'Price' : [],
     'Title' : [],
     'Date' : [],
 }
 
-for car in all_cars:
+for car in all_cars_list:
     car_info = car.find('p', class_="result-info")
 
     car_price = car.find('span', class_="result-price").text
@@ -64,7 +71,6 @@ for car in all_cars:
 
 cars_table = pd.DataFrame(cragslist_cars_data, columns=['Price', 'Title', 'Date'])
 cars_table.index = cars_table.index+1
-#print(cars_table)
+print(cars_table)
 
-cars_table.to_csv('LA_CL_cars.csv')
-"""
+cars_table.to_csv('All_cars_in_LA.csv')
